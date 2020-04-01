dfcc7571448fff88d1346cfffb6beac257b25e86
tomoki828 <pico4828@gmail.com>
2020-04-01 11:49:59 +0900

    [WIP]自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,42 @@
-$(function(){
+$(function(){  
+  var reloadMessages = function() {
+    var last_message_id = $('.chat-main__message-list__contents:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
   function buildHTML(message){
-    if ( message.image ) {
+    if ( message.content && message.image ) {
       var html =
-       `<div class="chat-main__message-list__contents">
+       `<div class="chat-main__message-list__contents" data-message-id=${message.id}>
         <div class="chat-main__message-list__contents__info">
         <div class="chat-main__message-list__contents__info--member-name">
-        ${message.user_name}
+          ${message.user_name}
         </div>
         <div class="chat-main__message-list__contents__info--date-and-time">
-        ${message.created_at}
+          ${message.created_at}
         </div>
         </div>
         <div class="chat-main__message-list__contents--message">
         <p class="chat-main__message-list__contents--message-content">
-        ${message.content}
+          ${message.content}
         </p>
         <img class="chat-main__message-list__contents--message-image" src=${message.image}>
         </div>
@@ -21,24 +44,24 @@ $(function(){
     return html;
   }else {
     var html =
-    `<div class="chat-main__message-list__contents">
+    `<div class="chat-main__message-list__contents" data-message-id=${message.id}>
      <div class="chat-main__message-list__contents__info">
      <div class="chat-main__message-list__contents__info--member-name">
-     ${message.user_name}
+      ${message.user_name}
      </div>
      <div class="chat-main__message-list__contents__info--date-and-time">
-     ${message.created_at}
+      ${message.created_at}
      </div>
      </div>
      <div class="chat-main__message-list__contents--message">
      <p class="chat-main__message-list__contents--message-content">
-     ${message.content}
+      ${message.content}
      </p>
      </div>
      </div>`
     return html;
   };
-}
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -61,4 +84,8 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
 });
